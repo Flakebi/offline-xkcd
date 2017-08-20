9c1b2078459a4f47dc0b7c78fd7e9484c8e7b32f
Flakebi <flakebi@t-online.de>
2017-08-20 14:40:44 +0200

    Git ready - working after 7.5 hours

--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,38 @@
 // Limit for error_chain
 #![recursion_limit = "1024"]
 
+extern crate bit_vec;
 #[macro_use]
 extern crate clap;
 #[macro_use]
 extern crate error_chain;
+extern crate rand;
+extern crate regex;
+extern crate reqwest;
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate serde;
 #[macro_use]
 extern crate serde_derive;
-extern crate serde;
+extern crate serde_json;
 
 use std::fs::File;
-use std::io::{Cursor, Read};
+use std::io::{BufReader, Cursor};
+use std::io::prelude::*;
 use std::net::IpAddr;
 use std::path::{Path, PathBuf};
 use std::result::Result;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
 
+use bit_vec::BitVec;
 use clap::{App, AppSettings, Arg, SubCommand};
-use rocket::{config, Data, State};
+use rand::Rng;
+use regex::Regex;
+use rocket::{config, State};
 use rocket::http::{ContentType, Status};
-use rocket::response::{self, content, Redirect, Responder, Response};
-use rocket::request::{Form, FromRequest, Request, Outcome};
+use rocket::response::{Redirect, Responder, Response};
+use rocket::request::Request;
 use rocket_contrib::Template;
 
 #[allow(unused_doc_comment)]
@@ -34,23 +43,88 @@ mod errors {
 	error_chain! {
 		foreign_links {
 			Io(::std::io::Error);
+			Reqwest(::reqwest::Error);
+			Serde(::serde_json::Error);
 		}
 	}
 }
 use errors::*;
+
+const CHAR_WHITELIST: &[char] = &['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_', '.'];
+/// The maximum number of matches for a search request.
+const MAX_MATCHES: usize = 150;
 
 struct Config {
 	address: IpAddr,
 	port: u16,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize, Debug)]
+struct Comic {
+	num: usize,
+	year: String,
+	month: String,
+	day: String,
+	link: String,
+	news: String,
+	transcript: String,
+	safe_title: String,
+	title: String,
+	alt: String,
+	img: String,
+}
+
+impl Comic {
+	fn get_img_path(&self) -> String {
+		let start = self.img.rfind('/').map(|i| i + 1).unwrap_or(0);
+		// Limit to allowed chars
+		let img = &self.img[start..];
+		let mut res = String::new();
+		for c in img.chars() {
+			if CHAR_WHITELIST.contains(&c) {
+				res.push(c);
+			}
+		}
+		res
+	}
+}
+
+#[derive(Serialize, Debug)]
 struct Xkcd {
+	comics: Vec<Option<Comic>>,
 }
 
 impl Xkcd {
 	fn new() -> Xkcd {
+		println!("Loading local comics");
+		let mut comics = Vec::new();
+		let file_count = Path::new("data").read_dir().map(|it| it.count()).unwrap_or(0);
+		// Load comics
+		for i in 0..file_count {
+			if let Ok(content) = File::open(format!("data/{}.json", i))
+			.and_then(|f| {
+				let mut reader = BufReader::new(f);
+				let mut content = String::new();
+				reader.read_to_string(&mut content).map(|_| content)
+			}) {
+				print!("\rLoading comic {}", i);
+				std::io::stdout().flush().unwrap();
+				let comic = serde_json::from_str(&content).unwrap();
+				comics.push(Some(comic));
+			} else {
+				comics.push(None);
+			}
+		}
+		// Remove trailing Nones
+		let mut i = comics.len();
+		while comics[i - 1].is_none() {
+			i -= 1;
+		}
+		comics.drain(i..);
+		println!("\rAll local comics loaded");
+
 		Xkcd {
+			comics: comics,
 		}
 	}
 }
@@ -130,12 +204,68 @@ fn main() {
 			};
 			Box::new(|| start_server(config, Xkcd::new()))
 		} else if let Some(_) = args.subcommand_matches("update") {
-			Box::new(|| println!("Updating xkcd"))
+			Box::new(|| update().unwrap())
 		} else {
 			Box::new(|| unreachable!("Invalid subcommand"))
 		}
 	};
 	fun()
+}
+
+fn download_image(comic: &Comic) -> Result<(), Error> {
+	// Get image
+	let img = if comic.img.starts_with("http:") {
+		format!("https{}", &comic.img[5..])
+	} else {
+		comic.img.to_string()
+	};
+	let mut buf = Vec::new();
+	reqwest::get(&img)?.read_to_end(&mut buf)?;
+	{
+		let mut f = File::create(format!("data/{}", comic.get_img_path())
+			.as_str())?;
+		f.write_all(&buf)?;
+	}
+	Ok(())
+}
+
+fn update() -> Result<(), Error> {
+	println!("Updating xkcd");
+	let xkcd = Xkcd::new();
+	// Get current id from /info.0.json
+	let mut result = String::new();
+	reqwest::get("https://xkcd.com/info.0.json")?.read_to_string(&mut result)?;
+	let latest_comic: Comic = serde_json::from_str(&result)?;
+
+	// Excluding the num because ids are one less (starting from 0 instead of 1)
+	for id in 0..latest_comic.num {
+		let _ = (|| -> errors::Result<()> {
+			if let Some(comic) = xkcd.comics.get(id).and_then(|c| c.as_ref()) {
+				let path = format!("data/{}", comic.get_img_path());
+				if !Path::new(&path).is_file() {
+					download_image(comic)?;
+				}
+			} else {
+				print!("\rDownloading {} of {}", id, latest_comic.num - 1);
+				std::io::stdout().flush().unwrap();
+				// Get image data from /<id + 1>/info.0.json
+				result.clear();
+				reqwest::get(format!("https://xkcd.com/{}/info.0.json", id + 1)
+					.as_str())?.read_to_string(&mut result)?;
+				let comic: Comic = serde_json::from_str(&result)?;
+				{
+					let mut f = File::create(format!("data/{}.json", id).as_str())?;
+					f.write_all(result.as_bytes())?;
+				}
+
+				// Get image
+				download_image(&comic)?;
+			}
+			Ok(())
+		})();
+	}
+	println!("                                              \n{} images up to date", latest_comic.num);
+	Ok(())
 }
 
 fn start_server(config: Config, xkcd: Xkcd) {
@@ -148,8 +278,11 @@ fn start_server(config: Config, xkcd: Xkcd) {
 		.unwrap();
 	let r = rocket::custom(rocket_config, false).mount("/", routes![
 		handle_index,
+		handle_comic,
+		handle_comic_path,
 		handle_static,
 		handle_search,
+		handle_random,
 	])
 		.manage(Arc::new(Mutex::new(xkcd)))
 		.attach(Template::fairing());
@@ -157,15 +290,97 @@ fn start_server(config: Config, xkcd: Xkcd) {
 }
 
 #[get("/static/<file..>")]
-fn handle_static<'r>(file: PathBuf) -> Option<WebFile> {
+fn handle_static(file: PathBuf) -> Option<WebFile> {
 	WebFile::new(Path::new("static").join(file))
 }
 
 #[get("/")]
-fn handle_index(xkcd: State<Arc<Mutex<Xkcd>>>) -> Template {
+fn handle_index(xkcd: State<Arc<Mutex<Xkcd>>>) -> Redirect {
 	let xkcd = xkcd.lock().unwrap();
-	let context = &*xkcd;
-	Template::render("index", context)
+	let id = xkcd.comics.len() - 1;
+	Redirect::to(format!("/comic/?id={}", id).as_str())
+}
+
+#[derive(FromForm)]
+struct IdForm {
+	id: Option<usize>
+}
+
+#[get("/comic?<id>")]
+fn handle_comic(xkcd: State<Arc<Mutex<Xkcd>>>, id: IdForm)
+	-> Result<Template, Redirect> {
+	#[derive(Serialize)]
+	struct ComicContext<'a> {
+		id: usize,
+		max_id: usize,
+		comic: &'a Comic,
+		img_path: String,
+	}
+
+	let xkcd = xkcd.lock().unwrap();
+	if let Some(id) = id.id {
+		if let Some(comic) = xkcd.comics.get(id).and_then(|c| c.as_ref()) {
+			let context = ComicContext {
+				id: id,
+				max_id: xkcd.comics.len() - 1,
+				comic: comic,
+				img_path: comic.get_img_path(),
+			};
+			Ok(Template::render("comic", &context))
+		} else {
+			Err(Redirect::to(format!("/comic/?id={}",
+				xkcd.comics.len() - 1).as_str()))
+		}
+	} else {
+		// Invalid id
+		Err(Redirect::to("/comic/?id=0"))
+	}
+}
+
+#[get("/comic/<file..>")]
+fn handle_comic_path(_xkcd: State<Arc<Mutex<Xkcd>>>, file: PathBuf)
+	-> Option<WebFile> {
+	WebFile::new(Path::new("data").join(file))
+}
+
+#[derive(Serialize)]
+struct ComicSearchResult<'a> {
+	id: usize,
+	comic: &'a Comic,
+	img_path: String,
+}
+
+struct SearchState<'a> {
+	results: Vec<ComicSearchResult<'a>>,
+	/// Comics that are already in the results
+	stored_comics: BitVec,
+}
+
+impl<'a> SearchState<'a> {
+	fn new(xkcd: &'a Xkcd) -> SearchState<'a> {
+		SearchState {
+			results: Vec::new(),
+			stored_comics: BitVec::from_elem(xkcd.comics.len(), false),
+		}
+	}
+
+	fn filter_comics(&mut self, xkcd: &'a Xkcd, f: &Fn(&Comic) -> bool) {
+		for (i, comic) in xkcd.comics.iter().enumerate() {
+			if let Some(ref comic) = *comic {
+				if !self.stored_comics[i] && f(comic) {
+					self.results.push(ComicSearchResult {
+						id: i,
+						comic: comic,
+						img_path: comic.get_img_path(),
+					});
+					self.stored_comics.set(i, true);
+				}
+				if self.results.len() >= MAX_MATCHES {
+					break;
+				}
+			}
+		}
+	}
 }
 
 #[derive(FromForm)]
@@ -174,8 +389,38 @@ struct SearchForm {
 }
 
 #[get("/search?<search>")]
-fn handle_search(xkcd: State<Arc<Mutex<Xkcd>>>, search: SearchForm) -> Template {
+fn handle_search(xkcd: State<Arc<Mutex<Xkcd>>>, search: SearchForm)
+	-> Template {
+	#[derive(Serialize)]
+	struct SearchContext<'a, 'b> {
+		search: &'a str,
+		max_id: usize,
+		results: Vec<ComicSearchResult<'b>>,
+	}
+
 	let xkcd = xkcd.lock().unwrap();
-	let context = &*xkcd;
-	Template::render("search", context)
+	let mut search_state = SearchState::new(&*xkcd);
+
+	// Search for matching words
+	let rgx = Regex::new(format!(r"\b{}\b", regex::escape(&search.q)).as_str()).unwrap();
+	search_state.filter_comics(&*xkcd, &|c| rgx.is_match(&c.title));
+	search_state.filter_comics(&*xkcd, &|c| rgx.is_match(&c.alt) || rgx.is_match(&c.transcript));
+	// Search for substrings
+	search_state.filter_comics(&*xkcd, &|c| c.title.contains(&search.q));
+	search_state.filter_comics(&*xkcd, &|c| c.alt.contains(&search.q) || c.transcript.contains(&search.q));
+
+	let context = SearchContext {
+		search: &search.q,
+		max_id: xkcd.comics.len() - 1,
+		results: search_state.results,
+	};
+	Template::render("search", &context)
+}
+
+#[get("/random")]
+fn handle_random(xkcd: State<Arc<Mutex<Xkcd>>>) -> Redirect {
+	let xkcd = xkcd.lock().unwrap();
+	let mut rng = rand::thread_rng();
+	let id = rng.gen_range(0, xkcd.comics.len());
+	Redirect::to(format!("/comic/?id={}", id).as_str())
 }
